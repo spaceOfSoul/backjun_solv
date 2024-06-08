8620ca363d139a214712c9b88c6dd7f7c48a233a
Changhyun Yu <85132981+spaceOfSoul@users.noreply.github.com>
2024-06-08 17:11:53 +0900

    [Gold IV] Title: 최단경로, Time: 104 ms, Memory: 9184 KB -BaekjoonHub

--- C++17/백준/Gold/1753. 최단경로/최단경로.cc
+++ C++17/백준/Gold/1753. 최단경로/최단경로.cc
@@ -1,59 +1,67 @@
 #include <iostream>
-#include <vector>
 #include <queue>
-#define inf 999999999
-using namespace std;
+#include <vector>
+#define INF 9999999
 
-int n,e,s;
-vector<pair<int, int>> graph[20000];
-int dist[20000];
+std::vector<std::pair<int, int>> graph[20000]; // w, v pair
+// w를 앞에 둠은 추후 우선순위 큐를 위함.
+int distance_table[20000]; // 시작 정점 k로부터의 거리 저장
 
-void djs(){
-  priority_queue<pair<int, int>> que;//distance,vertex
-  que.push({0,s});
-  dist[s] = 0;
+void djikstra(int start) {
+  std::priority_queue<std::pair<int, int>, std::vector<std::pair<int, int>>, std::greater<std::pair<int, int>>> que;
+  que.push({0, start}); // dist, vertex
+  distance_table[start] = 0;
 
-  while(!que.empty()){
-    pair<int, int> now = que.top();//distance,vertex
+  while (!que.empty()) {
+    std::pair<int, int> current = que.top();
+    
+    int current_node = current.second;
+    int current_dist = current.first;
+    
     que.pop();
 
-    if(dist[now.second] < -now.first)
-        continue;
 
-    for(pair<int, int> i : graph[now.second]){//i is graph(v,w)
-      int cost = -now.first+i.second;//will be distance
-      if(cost < dist[i.first]){
-        dist[i.first] = cost;
-        que.push({-cost, i.first});
+    if (distance_table[current_node] < current_dist)
+      continue;
+
+    for(auto pair : graph[current_node]){
+      int next = pair.second;
+      int next_dist = pair.first + current_dist;
+      
+      if(next_dist < distance_table[next]){
+        distance_table[next] = next_dist;
+        que.push({next_dist, next});
       }
     }
   }
 }
 
 int main() {
-  ios::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+  std::ios::sync_with_stdio(false);
+  std::cin.tie(NULL);
+  std::cout.tie(NULL);
+  int V, E;
+  int k;
+  int u, v, w;
 
-  cin >> n >> e;
-  cin >> s;
-  s--;
-  for(int i=0; i<n; i++)
-    dist[i] = inf;
-  
-  int u,v,w;
-  for(int i=0;i<e;i++){
-    cin >> u >> v >> w;
-    u--;v--;
-    graph[u].push_back({v,w});
+  std::cin >> V >> E;
+  std::cin >> k;
+
+  for (int i = 0; i < V; i++) {
+    distance_table[i] = INF;
   }
-  djs();
-  for(int i=0;i<n; i++){
-    if(i==s){
-      cout << 0;
-    }else if(dist[i] == inf){
-      cout << "INF";
-    }else{
-      cout << dist[i];
-    }
-    cout << "\n";
+
+  while (E--) {
+    std::cin >> u >> v >> w;
+    graph[u - 1].push_back({w, v - 1});
+  }
+
+  djikstra(k-1);
+
+  for(int i=0; i<V; i++){
+    if(distance_table[i] == INF)
+      std::cout << "INF\n";
+    else
+      std::cout << distance_table[i] << "\n";
   }
 }
