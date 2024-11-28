108fc4720949f6fd7ff7b9dc72c263415730e61e
Changhyun Yu <85132981+spaceOfSoul@users.noreply.github.com>
2024-11-28 12:16:10 +0900

    [Bronze V] Title: 직사각형, Time: 176 ms, Memory: 17632 KB -BaekjoonHub

---
+++ Java/백준/Bronze/27323. 직사각형/직사각형.java
@@ -0,0 +1,21 @@
+// don't place package name. 
+
+import java.io.*;
+
+import java.util.*; 
+
+// don't change 'Main' class name and  'public' accessor. 
+
+public class Main {
+    public static void main(String[] args) throws IOException { 
+
+        Scanner scanner = new Scanner(System.in); 
+
+        int a = scanner.nextInt(); 
+
+        int b = scanner.nextInt(); 
+
+        System.out.println(a * b); 
+
+    }
+}
