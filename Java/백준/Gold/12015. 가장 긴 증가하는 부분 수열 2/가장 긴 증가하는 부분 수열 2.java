897a931fa29a41c6689139d2136422d82aab41af
Changhyun Yu <85132981+spaceOfSoul@users.noreply.github.com>
2024-12-06 20:53:24 +0900

    [Gold II] Title: 가장 긴 증가하는 부분 수열 2, Time: 596 ms, Memory: 135548 KB -BaekjoonHub

--- Java/백준/Gold/12015. 가장 긴 증가하는 부분 수열 2/가장 긴 증가하는 부분 수열 2.java
+++ Java/백준/Gold/12015. 가장 긴 증가하는 부분 수열 2/가장 긴 증가하는 부분 수열 2.java
@@ -1,6 +1,9 @@
+// don't place package name. 
+
 import java.io.*;
 import java.util.*;
 
+// don't change 'Main' class name and  'public' accessor. 
 
 public class Main {
     public static void main(String[] args) throws IOException { 
