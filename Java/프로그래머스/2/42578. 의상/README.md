e91a6b90ccccbe0db1deab6bf3330429dec064bd
Changhyun Yu <85132981+spaceOfSoul@users.noreply.github.com>
2024-12-16 18:09:27 +0900

    [level 2] Title: 의상, Time: 0.16 ms, Memory: 88.9 MB -BaekjoonHub

--- Java/프로그래머스/2/42578. 의상/README.md
+++ Java/프로그래머스/2/42578. 의상/README.md
@@ -1,10 +1,10 @@
 # [level 2] 의상 - 42578 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42578?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42578) 
 
 ### 성능 요약
 
-메모리: 74.4 MB, 시간: 1.29 ms
+메모리: 88.9 MB, 시간: 0.16 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 10월 28일 19:12:20
+2024년 12월 16일 18:09:26
 
 ### 문제 설명
 
--- Java/프로그래머스/2/42578. 의상/의상.java
+++ Java/프로그래머스/2/42578. 의상/의상.java
@@ -3,16 +3,16 @@ import java.util.*;
 
 class Solution {
     public int solution(String[][] clothes) {
-        // 종류별로 옷 개수를 세기
+        int answer = 1;
         Map<String, Integer> closet = new HashMap<>();
         for (String[] cloth : clothes) {
             closet.put(cloth[1], closet.getOrDefault(cloth[1], 0) + 1);
         }
-
-        int answer = closet.values().stream()
-                           .map(count -> count + 1) 
-                           .reduce(1, (a, b) -> a * b); // 모든 경우의 수를 곱함
-
-        return answer - 1; // 아무것도 선택하지 않는 경우 제거
+        
+        for (int count : closet.values()) {
+            answer *= (count + 1);
+        }
+        answer -= 1;
+        return answer; 
     }
 }
